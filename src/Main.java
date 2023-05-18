e035833a657825c80042666c94b9a68feae5b6cf
joaozinhozo12345678910 <js0607615@gmail.com>
2023-05-18 08:28:56 -0300

    eu entendi que oum objeto chamado pessoa instaciamos o objeto definido e sobre nome desse projeto aprendi támbem que não é só copiar e colar e sim buscar

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,19 @@
 public class Main {
     public static void main(String[] args) {
-        
+        // Aula de Orientações á Objetos
+        // Declarar obejetos
+        Pessoa adao;
+        // Instanciar objetos
+        adao = new Pessoa();
+        Pessoa eva = new Pessoa();
+        // Definição do foemato
+        adao.nome = "Adão";
+        adao.sobrenome = "Silva";
+        eva.nome = "Eva";
+        eva.sobrenome = "Silva";
+        // Acionar comportamnto
+        adao.falar();
+        eva.falar();
+
     }
 }
