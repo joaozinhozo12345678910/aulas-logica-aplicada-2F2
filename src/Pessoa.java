702215a434a4eda1fb6a2726b9727ec0237dbadc
joaozinhozo12345678910 <js0607615@gmail.com>
2023-05-18 08:28:56 -0300

    eu entendi que oum objeto chamado pessoa instaciamos o objeto definido e sobre nome desse projeto aprendi támbem que não é só copiar e colar e sim buscar

---
+++ src/Pessoa.java
@@ -0,0 +1,9 @@
+public class Pessoa {
+    // Atributo da classe
+    String nome;
+    String sobrenome;
+    // Método da classe
+    public void falar(){
+        System.out.println("falei");
+    }
+}
